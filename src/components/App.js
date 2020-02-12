da4c9a1e9f9087e711242082593d50c24e62aef4
okFox <smayfield@gmail.com>
2020-02-12 15:42:33 -0800

    tests passing.  reducer and actions written

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import Moods from '../containers/Moods';
+// import {
+//   BrowserRouter as Router,
+//   Switch,
+//   Route
+// } from 'react-router-dom';
 
 
 export default function App() {
--- src/containers/Moods.test.js
+++ src/containers/Moods.test.js
@@ -37,23 +37,23 @@ describe('Moods', () => {
     });
 
     it('is very angry if tired and hungry', () => {
-      expect(getFace({ coffees: 0, naps: 0, snacks: 0 })).toEqual('🤬');
+      expect(getFace({ coffees: 0, naps: 0, snacks: 0 })).toEqual('😠');
     });
 
     it('is sick if hyper and hungry', () => {
-      expect(getFace({ coffees: 4, snacks: 0 })).toEqual('🤮');
+      expect(getFace({ coffees: 4, snacks: 0 })).toEqual('😱');
     });
 
     it('is sleeping if tired', () => {
-      expect(getFace({ coffees: 4, snacks: 0 })).toEqual('🤮');
+      expect(getFace({ coffees: 4, snacks: 0 })).toEqual('😱');
     });
 
     it('is crazy if hyper', () => {
-      expect(getFace({ coffees: 4, naps: 0, snacks: 0, studies: 0 })).toEqual('🤮');
+      expect(getFace({ coffees: 4, naps: 0, snacks: 0, studies: 0 })).toEqual('😱');
     });
 
     it('is mindblown if educated', () => {
-      expect(getFace({ coffees: 1, naps: 0, snacks: 1, studies: 3 })).toEqual('🤯');
+      expect(getFace({ coffees: 1, naps: 0, snacks: 1, studies: 3 })).toEqual('😲');
     });
 
     it('is angry if hungry', () => {
